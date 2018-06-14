ff81881ef7913f401f7a48492259c42884ec8f8c
Bensen <bensen4emsen@gmail.com>
2018-06-14 16:31:33 +0200

    gradientvar

--- js/main.js
+++ js/main.js
@@ -119,6 +119,20 @@ window.onload = function(){
     .attr("stroke-width", width[count]*width_multiplicator);
    
   }
+
+  /* Adjust Gradient*/
+  $('#wir').hover(function(){
+    $( '#wir' ).on( "mousemove", function( event ) {
+      let height = $('#landing-intro').height();
+      let spektrum = $('#stripes').height();
+      let y= event.pageY-height;
+      let relcursor= 1- y/spektrum;
+      if(relcursor>.35 && relcursor<.65){
+        let gradient = d3.select('#wir #stripes defs #nightsky');
+        gradient.select('stop#mid').attr('offset', relcursor);
+      }
+    });
+  });
  
   };
 
@@ -168,37 +182,6 @@ $('#mail-button').click(function() {
 });
 
 
-/* RETURN FROM MAIL Effect*/
-
-/* $('.b-btn').click(function(){
-  $('#section-one').css({transform:"translateX(100%) translateY(-50%)"});
-  var frontSlideBack = window.setTimeout(function(){
-    $('#section-one').css({transform:"translateX(-50%) translateY(-50%)"});
-    $('#section-one').removeClass('slide-in');
-    $('.front-content').removeClass('slide-out');
-  }, 1000);  
-}) */
-
-$('#leaveLetterIcon, #leaveLetterIcon2').click(function(){
-  $('#section-one').removeClass('slide-in');
-  //$('#leaveLetterIcon').removeClass('slide-in');
-  var frontSlideBack = window.setTimeout(function(){
-  $('.front-content').removeClass('slide-out')}, 1000);
-
-});
-
-/* Flip Flipcard  */
-$('#marker').click(function(){
- $('#flipcard').css({transform:"rotateY(180deg)"});
- updateSectionOneSize();
-});
-
-$('#x-icon').click(function(){
-  $('#flipcard').css({transform:"rotateY(0deg)"});
-  updateSectionOneSize();
-/*   $('.backside').css({display:"none"});
- */
-});
 
 
 
